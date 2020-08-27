6cc7972981b01b3237aa8bfd8f6253aea74dfe12
Cooper Saye <coopersaye@gmail.com>
2020-08-27 12:51:21 -0700

    Set up 8 bar UI.

---
+++ Assets/RandoPiano/Scripts/NoteGenerator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace RandoPiano
+{
+    public class NoteGenerator : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/RandoPiano/Scripts/NotePlayer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace RandoPiano
+{
+    public class NotePlayer : MonoBehaviour
+    {
+        
+    }
+}
