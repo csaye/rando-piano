b2196c419bd41b64644561da3990c982a75c9972
Cooper Saye <coopersaye@gmail.com>
2020-08-31 10:27:53 -0700

    Implement tempo slider.

---
+++ Assets/RandoPiano/Scripts/UI/TempoSlider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace RandoPiano
+{
+    [RequireComponent(typeof(Slider))]
+    public class TempoSlider : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private NoteGenerator noteGenerator = null;
+
+        private Slider slider;
+
+        private void Start()
+        {
+            slider = GetComponent<Slider>();
+        }
+
+        public void UpdateTempo()
+        {
+            noteGenerator.noteSpeed = slider.value;
+        }
+    }
+}
